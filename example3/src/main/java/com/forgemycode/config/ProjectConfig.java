777bea22a2ffa8f41ac871913be125cd2545d84e
IamRama <javabyram1995@gmail.com>
2024-12-23 17:57:17 +0530

    example 3,4,5

---
+++ example3/src/main/java/com/forgemycode/config/ProjectConfig.java
@@ -0,0 +1,44 @@
+package com.forgemycode.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.forgemycode.beans.Vehicle;
+
+/*
+Spring @Configuration annotation is part of the spring core framework.
+Spring Configuration annotation indicates that the class has @Bean definition
+methods. So Spring container can process the class and generate Spring Beans
+to be used in the application.
+* */
+@Configuration
+public class ProjectConfig {
+
+
+    /*
+    @Bean annotation, which lets Spring know that it needs to call
+    this method when it initializes its context and adds the returned
+    value to the context.
+    * */
+	@Bean(name="vehicle1")
+    Vehicle vehicle1() {
+        var veh = new Vehicle();
+        veh.setName("Audi");
+        return veh;
+    }
+
+    @Bean(value="vehicle")
+    Vehicle vehicle2() {
+        var veh = new Vehicle();
+        veh.setName("Honda");
+        return veh;
+    }
+
+    @Bean("vehicle3")
+    Vehicle vehicle3() {
+        var veh = new Vehicle();
+        veh.setName("Ferrari");
+        return veh;
+    }
+
+}
---
+++ example5/src/main/java/com/forgemycode/Example5.java
@@ -0,0 +1,27 @@
+package com.forgemycode;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.forgemycode.beans.Vehicle;
+import com.forgemycode.config.ProjectConfig;
+
+public class Example5 {
+
+    public static void main(String[] args) {
+
+       
+        /*
+        The var keyword was introduced in Java 10. Type inference is used in
+        var keyword in which it detects automatically the datatype of a variable
+        based on the surrounding context.
+        * */
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        //Vehicle veh = context.getBean(Vehicle.class);
+        Vehicle veh = context.getBean(Vehicle.class);
+        System.out.println("Vehicle name from Spring Context is: " + veh.getName());
+        veh.printHello();
+
+       
+    }
+}
